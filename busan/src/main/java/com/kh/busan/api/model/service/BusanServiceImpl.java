831e849edbb51288d74736137205fc4bc9de425d
hyunbin <yuyuuri111@gmail.com>
2025-01-22 14:05:18 +0900

    Init Busan

---
+++ busan/src/main/java/com/kh/busan/api/model/service/BusanServiceImpl.java
@@ -0,0 +1,11 @@
+package com.kh.busan.api.model.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class BusanServiceImpl implements BusanService {
+
+}
---
+++ busan/src/main/java/com/kh/busan/api/model/vo/CommentDTO.java
@@ -0,0 +1,5 @@
+package com.kh.busan.api.model.vo;
+
+public class CommentDTO {
+
+}
