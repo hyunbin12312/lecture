c595ce126398e824aeb4e4a75d692bd913927d3c
hyunbin12312 <yuyuuri111@gmail.com>
2025-01-25 23:23:57 +0900

    25-01-23 1 end

---
+++ secom/src/main/java/com/kh/secom/auth/service/AuthenticationService.java
@@ -0,0 +1,13 @@
+package com.kh.secom.auth.service;
+
+import java.util.Map;
+
+import com.kh.secom.member.model.vo.MemberDTO;
+
+public interface AuthenticationService {
+
+	// AccessToken, RefreshToken 두개를 한곳에 담아서 보내야하기때문에 Map 사용
+	
+	Map<String, String> login(MemberDTO requestMember);
+	
+}
